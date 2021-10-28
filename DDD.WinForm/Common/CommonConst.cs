a0796b17354d7b6c7f13d33fef707680baef3c5e
h-uchida <h-uchida@plamed.com>
2021-10-28 11:39:40 +0900

    共通化

---
+++ DDD.WinForm/Common/CommonConst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.WinForm.Common
+{
+    public static class CommonConst
+    {
+        public const string ConnectionString = @"Data Source=ddd.sqlite3;Version=3;";
+        public const string TemperatureUnitName = "℃";
+        public const int TemperatureDecimalPoint = 2;
+
+    }
+}
