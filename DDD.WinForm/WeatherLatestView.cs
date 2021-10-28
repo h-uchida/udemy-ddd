25240e8c98a6925fcc1ac0fe7491fdd6d47585f7
h-uchida <h-uchida@plamed.com>
2021-10-28 11:20:16 +0900

    BADコードの実装まで

--- DDD.WinForm/WeatherLatestView.cs
+++ DDD.WinForm/WeatherLatestView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace DDD.WinForm
 {
     public partial class WeatherLatestView : Form
     {
+        private readonly string ConnectionString = @"Data Source=ddd.sqlite3;Version=3;";
+
         public WeatherLatestView()
         {
             InitializeComponent();
@@ -19,6 +22,26 @@ namespace DDD.WinForm
 
         private void LatestButton_Click(object sender, EventArgs e)
         {
+            var sql = @"select DataDate, Condition, Temperature from Weather where AreaId = @AreaId order by DataDate desc LIMIT 1";
+
+            DataTable dt = new DataTable();
+            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var command = new SQLiteCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@AreaId", this.AreaIdTextBox.Text);
+                using (var adapter = new SQLiteDataAdapter(command))
+                {
+                    adapter.Fill(dt);
+                }
+            }
+
+            if (dt.Rows.Count > 0)
+            {
+                DataDateLabel.Text = dt.Rows[0]["DataDate"].ToString();
+                ConditionLabel.Text = dt.Rows[0]["Condition"].ToString();
+                TemperatureLabel.Text = Math.Round(Convert.ToSingle(dt.Rows[0]["Temperature"]), 2) + "℃";
+            }
 
         }
     }
